b1dacfbdf04ded43130046f34c5827c7067c79a8
NoName <contact@cedricduffournet.com>
2019-07-07 13:23:35 +0200

    Return http bad request if civility can not be deleted

--- app/src/Migrations/Version20190707102406.php
+++ app/src/Migrations/Version20190707102406.php
@@ -12,25 +12,25 @@ use Doctrine\Migrations\AbstractMigration;
  */
 final class Version20190707102406 extends AbstractMigration
 {
-    public function getDescription() : string
+    public function getDescription(): string
     {
         return '';
     }
 
-    public function up(Schema $schema) : void
+    public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE fos_user DROP FOREIGN KEY FK_957A647923D6A298');
         $this->addSql('ALTER TABLE fos_user CHANGE civility_id civility_id INT NOT NULL');
         $this->addSql('ALTER TABLE fos_user ADD CONSTRAINT FK_957A647923D6A298 FOREIGN KEY (civility_id) REFERENCES civility (id)');
     }
 
-    public function down(Schema $schema) : void
+    public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+        $this->abortIf('mysql' !== $this->connection->getDatabasePlatform()->getName(), 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE fos_user DROP FOREIGN KEY FK_957A647923D6A298');
         $this->addSql('ALTER TABLE fos_user CHANGE civility_id civility_id INT DEFAULT NULL');
