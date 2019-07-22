ba567cd1b1a9ad9aa72324f9500788e674414878
NoName <contact@cedricduffournet.com>
2019-07-22 16:50:31 +0200

    Add delete civility error to view instance

--- app/src/Controller/CivilityController.php
+++ app/src/Controller/CivilityController.php
@@ -209,7 +209,7 @@ class CivilityController extends AbstractFOSRestController
         try {
             $this->civilityService->deleteCivility($civility);
         } catch (ForeignKeyConstraintViolationException $e) {
-            throw new BadRequestHttpException('You can not delete this entity');
+            return $this->view(['message' => 'You can not delete this entity !'], Response::HTTP_BAD_REQUEST);
         }
 
         return $this->view([], Response::HTTP_NO_CONTENT);
